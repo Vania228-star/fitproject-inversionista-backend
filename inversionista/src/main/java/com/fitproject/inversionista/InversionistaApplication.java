3d1d3ad15676889c4a740f75cb2f851d28489330
Vania <Van.carvajal@duocuc.cl>
2026-05-10 21:35:12 -0400

    first commit

---
+++ inversionista/src/main/java/com/fitproject/inversionista/InversionistaApplication.java
@@ -0,0 +1,13 @@
+package com.fitproject.inversionista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InversionistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InversionistaApplication.class, args);
+	}
+
+}
---
+++ inversionista/src/test/java/com/fitproject/inversionista/InversionistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitproject.inversionista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InversionistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
