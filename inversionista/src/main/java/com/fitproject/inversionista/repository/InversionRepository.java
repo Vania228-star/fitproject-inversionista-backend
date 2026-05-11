539f7f3ff6cb5452fd6894bc471839ea064e1f7c
Vania <Van.carvajal@duocuc.cl>
2026-05-10 23:05:35 -0400

    Creación de los métodos personalizados en el repositorio

--- inversionista/src/main/java/com/fitproject/inversionista/repository/InversionRepository.java
+++ inversionista/src/main/java/com/fitproject/inversionista/repository/InversionRepository.java
@@ -1,5 +1,7 @@
 package com.fitproject.inversionista.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,11 @@ import com.fitproject.inversionista.model.Inversion;
 @Repository
 public interface InversionRepository extends JpaRepository<Inversion, Long> {
     
+    List<Inversion> findByIdUsuario(Long idUsuario);
+
+    List<Inversion> findByIdProyecto(Long idProyecto);
+
+    List<Inversion> findByMontoInvertidoGreaterThan(Double monto);
+
+    List<Inversion> findByFechaInversionBetween(java.time.LocalDateTime inicio, java.time.LocalDateTime fin);
 }
