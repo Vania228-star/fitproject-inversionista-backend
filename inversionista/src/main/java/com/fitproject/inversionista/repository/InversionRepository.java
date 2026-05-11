7e37b3b28acc977bb9317169305490cfff20c36a
Vania <Van.carvajal@duocuc.cl>
2026-05-10 22:04:00 -0400

    creación del repositorio

--- inversionista/src/main/java/com/fitproject/inversionista/repository/InversionRepository.java
+++ inversionista/src/main/java/com/fitproject/inversionista/repository/InversionRepository.java
@@ -1,5 +1,11 @@
 package com.fitproject.inversionista.repository;
 
-public class InversionRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fitproject.inversionista.model.Inversion;
+
+@Repository
+public interface InversionRepository extends JpaRepository<Inversion, Long> {
     
 }
